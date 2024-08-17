e51d2ea7d48cc0bae4bf4a8b9f5ec72ef3116dd6
Parsa_Shabanpour <parsash1380@gmail.com>
2024-08-17 10:27:18 +0330

    implement SpanishGreetingService

---
+++ src/main/java/com/example/SpanishGreetingService.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import com.example.annotations.Component;
+import com.example.annotations.Qualifier;
+import com.example.enums.Scope;
+import com.example.interfaces.IGreetingService;
+
+@Component(scope = Scope.PROTOTYPE)
+@Qualifier("spanishGreeting")
+public class SpanishGreetingService implements IGreetingService {
+    @Override
+    public void greet(String name) {
+        System.out.println("Hola, " + name + "!");
+    }
+}
